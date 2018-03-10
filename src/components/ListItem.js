dc2f5f687da9b9b2520d9374822de4742ce54779
Quang Nguyen <qtn2@njit.edu>
2018-03-10 11:18:54 -0500

    initial commit

---
+++ src/components/ListItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common';
+
+class ListItem extends Component {
+    render() {
+
+        const{ titleStyles } = styles;
+
+        return (
+            <CardSection>
+                <Text style={titleStyles}>{this.props.library.title}</Text>
+            </CardSection>
+        )
+    }
+}
+
+const styles = {
+    titleStyles: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+}
+
+export default ListItem;
---
+++ src/components/common/Card.js
@@ -0,0 +1,33 @@
+//Import librraies for making components
+import React from 'react';
+import { View } from 'react-native';
+
+
+//Making components
+const Card = (props) => {
+    return (
+        <View style={styles.containerStyle}>
+            {props.children}
+        </View>
+    )
+}
+
+const styles = {
+    containerStyle: {
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10        
+    }
+}
+
+//Make components available to other parts of application
+export { Card };
